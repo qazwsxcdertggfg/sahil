7e8954d9e821d0cba3a43e2f1d015206c0a6f616
qazwsxcdertggfg <125578964+qazwsxcdertggfg@users.noreply.github.com>
2023-02-17 22:43:19 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Sahil123"
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+  
+  terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "vm-web" {
+  
+  ami           = "ami-0cca134ec43cf708f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server for web"
+    Env = "dev"
+  }
+}
